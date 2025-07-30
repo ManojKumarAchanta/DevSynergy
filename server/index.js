27e57500044c792e871034beb069d3ff7d29ba26
ManojKumarAchanta <manojachanta7172@gmail.com>
2025-07-30 22:09:37 +0530

    changed api endpoint

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: '*',
+    origin: ['http://localhost:5173', 'http://localhost:8080'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   })
